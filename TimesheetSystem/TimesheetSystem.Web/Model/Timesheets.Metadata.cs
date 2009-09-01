d50a070026914350da42d81a89484d9e8c1bee8f
Mark Thompson <mathomps@internode.on.net>
2009-09-01 01:16:04 +0000

    Initial check-in
    git-svn-id: svn://webhost01/Personal%20Projects@259 ebe4f960-59b0-3b4e-9416-0a66d08d054f

---
+++ TimesheetSystem/TimesheetSystem.Web/Model/Timesheets.Metadata.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TimesheetSystem.Web.Model
+{
+
+    // --  Timesheets Table  --
+
+    [MetadataType(typeof(Timesheet_Metadata))]
+    public partial class Timesheet
+    {
+    }
+
+    public class Timesheet_Metadata
+    {
+        // Hide the ID column, as it is auto-generated
+        [ScaffoldColumn(false)]
+        public object id { get; set; }
+    }
+
+
+    // --  Clients Table  --
+
+    [MetadataType(typeof(Client_Metadata))]
+    public partial class Client
+    {
+    }
+
+    public class Client_Metadata
+    {
+        // Hide the ID column, as it is auto-generated
+        [ScaffoldColumn(false)]
+        public object id { get; set; }
+    }
+
+}
