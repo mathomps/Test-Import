54de19bfca6169e1b448731c86559e20b0a09cb0
Mark Thompson <mathomps@internode.on.net>
2009-11-15 05:33:53 +0000

    Implemented custom view for Timesheet table, and sorted it by Date Entered descending.
    git-svn-id: svn://webhost01/Personal%20Projects@284 ebe4f960-59b0-3b4e-9416-0a66d08d054f

--- TimesheetSystem/TimesheetSystem.Web/Model/Timesheets.Metadata.cs
+++ TimesheetSystem/TimesheetSystem.Web/Model/Timesheets.Metadata.cs
@@ -10,28 +10,42 @@ namespace TimesheetSystem.Web.Model
 
     // --  Timesheets Table  --
 
+    [DisplayColumn("DateEntered", "DateEntered", true)]
     [MetadataType(typeof(Timesheet_Metadata))]
     public partial class Timesheet
     {
+
+        public double TotalAmount
+        {
+            get { return HourlyRate * Quantity; }
+            set
+            {
+            }
+        }
+
     }
 
     public class Timesheet_Metadata
     {
         // Hide the ID column, as it is auto-generated
         [ScaffoldColumn(false)]
-        public object id {get; set;}
+        public object id { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:c}")]
         [DisplayName("Hourly Rate")]
-        public object HourlyRate {get; set;}
+        public object HourlyRate { get; set; }
 
         [DisplayName("Invoice Number")]
-        public object InvoiceNumber {get; set;}
+        public object InvoiceNumber { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:d}")]
         [DisplayName("Date Entered")]
-        public object DateEntered {get; set;}
+        public object DateEntered { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        [DisplayName("Total Amount")]
+        [ScaffoldColumn(true)]
+        public object TotalAmount { get; set; }
     }
 
 
